685faef663efe71c257050d585985e68f65edfdd
LorenaModica <lorenamodica@gmail.com>
2022-06-28 16:56:23 +0200

    fixato _hll_rank

--- utils.lua
+++ utils.lua
@@ -39,5 +39,20 @@ function toBinString(s)
   return binary
 end
 
+function toDecBin(bin)
+
+	bin = string.reverse(bin)
+	local sum = 0
+
+	for i = 1, string.len(bin) do
+  		num = string.sub(bin, i,i) == "1" and 1 or 0
+		sum = sum + num * math.pow(2, i-1)
+	end
+	
+	return sum
+
+end
+
+
 
 
