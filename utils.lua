77ed32e7eac655778115af6bdf4b078395b502a5
LorenaModica <lorenamodica@gmail.com>
2022-06-29 18:31:50 +0200

    pulizia codice

--- utils.lua
+++ utils.lua
@@ -2,41 +2,44 @@
 -- @return true or false
 function check_ipv4(ipv4_address)
 	
-  if type(ipv4_address) ~= "string" then return false end
+	if type(ipv4_address) ~= "string" then return false end
 
-  -- check for format 1.11.111.111 for ipv4
-  local chunks = {ipv4_address:match("^(%d+)%.(%d+)%.(%d+)%.(%d+)$")}
-  if #chunks == 4 then
-    for _,v in pairs(chunks) do
-      if tonumber(v) > 255 then return false end
-    end
-    return true
-  end
+  	-- check for format 1.11.111.111 for ipv4
+  	local chunks = {ipv4_address:match("^(%d+)%.(%d+)%.(%d+)%.(%d+)$")}
+  	
+	if #chunks == 4 then
+    	for _,v in pairs(chunks) do
+      		if tonumber(v) > 255 then return false end
+    	end
+		return true
+  	end
 
 end
 
 --code from https://stackoverflow.com/questions/17650169/binary-code-to-string
 function toBinString(s)
-  bytes = {string.byte(s,i,string.len(s))}
-  binary = ""
+	bytes = {string.byte(s,i,string.len(s))}
+  	binary = ""
 
-  for i,number in ipairs(bytes) do
-    c = 0
+  	for i,number in ipairs(bytes) do
+    	c = 0
 
-    while(number>0) do
-      binary = (number%2)..binary
-      number = number - (number%2)
-      number = number/2
-      c = c+1
-    end
+    	while(number>0) do
+      		binary = (number%2)..binary
+      		number = number - (number%2)
+      		number = number/2
+      		c = c+1
+    	end
 
-    while(c<8) do
-      binary = "0"..binary
-      c = c+1
-    end
-  end
+    	while(c<8) do
+      		binary = "0"..binary
+      		c = c+1
+    	end
+	
+	end
 
   return binary
+
 end
 
 function toDecBin(bin)
--- wiresharkListener.lua
+++ wiresharkListener.lua
@@ -125,8 +125,8 @@ register_menu("Host info/Host stats",host_info,MENU_TOOLS_UNSORTED)
 
 -- Notify the user that the menu was created
 if gui_enabled() then
-   local splash = TextWindow.new("Hello!");
-   splash:set("Wireshark has been enhanced with a new feature.\n")
-   splash:append("Go to Tools -> Host info -> Host stats and check it out!")
+	local splash = TextWindow.new("Hello!");
+	splash:set("Wireshark has been enhanced with a new feature.\n")
+    splash:append("Go to Tools -> Host info -> Host stats and check it out!")
 end
 
